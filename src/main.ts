f025c9cc9b2ce1402533d30b75a4d51e26333803
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2025-06-02 13:47:48 +0000

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as hbs from 'hbs';
+import * as showdown from 'showdown';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -16,7 +17,11 @@ async function bootstrap() {
   });
   hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
+  });
+  hbs.registerHelper('md', function(context) {
+    const converter = new showdown.Converter();
+    return converter.makeHtml(context);
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
