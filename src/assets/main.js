036c66f4e17718006c6774e000ac12b0758a7f00
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2025-06-09 12:58:18 +0000

    fix(global): fix petite-vue build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,36 +1,36 @@
 import * as PetiteVue from 'petite-vue';
 
 PetiteVue.createApp({
-    card: function() {
-      return {
-        $template: '#card-template',
-        card: null,
-        template: '',
-        loadAndRenderCard(id) {
-          console.log(`Load card ${id}`);
-          fetch(`https://game-assets.thefirstspine.fr/cards/${id}`)
-            .then((response) => {
-              response.json().then((json) => {
-                console.log(`Card loaded:`,  json);
-                this.card = json;
-              });
+    card: PetiteVue.reactive({
+      $template: '#card-template',
+      card: null,
+      template: '',
+      loadAndRenderCard(id) {
+        console.log(`Load card ${id}`);
+        fetch(`https://game-assets.thefirstspine.fr/cards/${id}`)
+          .then((response) => {
+            response.json().then((json) => {
+              console.log(`Card loaded:`,  json);
+              this.card = json;
             });
-        },
-        renderCardBase64(str) {
-          this.card = JSON.parse(atob(str));
-        }
-      }
-    },
-    toggles: {},
-    toggleOn(label) {
-      this.toggles[label] = true;
-    },
-    toggleOff(label) {
-      this.toggles[label] = false;
-    },
-    isToggled(label) {
-      return this.toggles[label] || false;
-    },
+          });
+      },
+      renderCardBase64(str) {
+        this.card = JSON.parse(atob(str));
+      },
+    }),
+    app: PetiteVue.reactive({
+      toggles: {},
+      toggleOn(label) {
+        this.toggles[label] = true;
+      },
+      toggleOff(label) {
+        this.toggles[label] = false;
+      },
+      isToggled(label) {
+        return this.toggles[label] || false;
+      },
+    })
 }).directive('click-outside', function (ctx) {
   document.body.addEventListener('click', function (event) {
     // here I check that click was outside the el and his children
