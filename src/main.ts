f3049418442e9fb3b2e16f26f48f9307fadc3259
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2025-06-10 13:07:16 +0000

    feat(app): init i18n

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ErrorFilter, LogsService, RequestsLoggerMiddleware } from '@thefirstspine/logs-nest';
 import session from 'express-session';
 import flash from 'connect-flash';
+import { I18n } from 'i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -26,6 +27,13 @@ async function bootstrap() {
   );
   app.use(flash());
 
+  const i18n = new I18n();
+  i18n.configure({
+    locales: ['en', 'fr'],
+    directory: join(__dirname, 'i18n'),
+  });
+  i18n.setLocale('fr');
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
@@ -34,18 +42,19 @@ async function bootstrap() {
   hbs.registerHelper('json', function(context) {
     return JSON.stringify(context);
   });
-  hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+  hbs.registerHelper('ifEquals', function(arg1, arg2, context) {
+    return (arg1 == arg2) ? context.fn(this) : context.inverse(this);
   });
   hbs.registerHelper('md', function(context) {
     const converter = new showdown.Converter({simpleLineBreaks: true});
     return converter.makeHtml(context);
   });
-  hbs.registerHelper('env', function(arg1, options) {
+  hbs.registerHelper('env', function(arg1, context) {
     return process.env[arg1];
   });
-  hbs.registerHelper('btoa', function(context) {
-    btoa(context);
+  hbs.registerHelper('i18n', function(arg1, context) {
+    console.log({arg1, res: i18n.__(arg1), locale: i18n.getLocale()});
+    return i18n.__(arg1);
   });
 
   await app.listen(process.env.PORT ?? 3000);
